735ea44eac9ec28f04339d5d28e227ceda90519d
Lazar Kevin <lazar.kevin.krisztofer@diak.szbi-pg.hu>
2026-04-19 18:47:43 +0200

    Update event dates in Jelenlegi.sql

--- Jelenlegi.sql
+++ Jelenlegi.sql
@@ -38,6 +38,6 @@ INSERT INTO Users (Name, Password, Email, EventId, ImgNumber)
 VALUES ('Admin', 'biztonsagos_jelszo_123', 'admin@example.com', NULL, '1.png');
 
 INSERT INTO Eventt (Name, Descr, Datetol, Dateig, Categ, Important, Wheres)
-VALUES ('Iskolai kirándulás', 'Kirándulás a közeli erdőbe', '2026-09-15 08:00:00', '2024-09-15 18:00:00', 'Kültéri', TRUE, 'Erdő');
-VALUES ('Tudományos előadás', 'Előadás a fizika világáról', '2026-10-01 14:00:00', '2024-10-01 16:00:00', 'Beltéri', FALSE, 'Aula');
-VALUES ('Sportnap', 'Egész napos sportesemény a pályán', '2026-11-20 09:00:00', '2024-11-20 17:00:00', 'Kültéri', TRUE, 'Sportpálya');
+VALUES ('Iskolai kirándulás', 'Kirándulás a közeli erdőbe', '2026-09-15 08:00:00', '2026-09-15 18:00:00', 'Kültéri', TRUE, 'Erdő');
+VALUES ('Tudományos előadás', 'Előadás a fizika világáról', '2026-10-01 14:00:00', '2026-10-01 16:00:00', 'Beltéri', FALSE, 'Aula');
+VALUES ('Sportnap', 'Egész napos sportesemény a pályán', '2026-11-20 09:00:00', '2026-11-20 17:00:00', 'Kültéri', TRUE, 'Sportpálya');
