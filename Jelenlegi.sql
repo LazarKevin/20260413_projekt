a003a35514d3e8ed83e2f7f5985f3534453d3b46
Lazar Kevin <lazar.kevin.krisztofer@diak.szbi-pg.hu>
2026-04-20 10:16:22 +0200

    Update Jelenlegi.sql

--- Jelenlegi.sql
+++ Jelenlegi.sql
@@ -38,6 +38,12 @@ INSERT INTO Users (Name, Password, Email, EventId, ImgNumber)
 VALUES ('Admin', 'biztonsagos_jelszo_123', 'admin@example.com', NULL, '1.png');
 
 INSERT INTO Eventt (Name, Descr, Datetol, Dateig, Categ, Important, Wheres)
-VALUES ('Iskolai kirándulás', 'Kirándulás a közeli erdőbe', '2026-09-15 08:00:00', '2026-09-15 18:00:00', 'Kültéri', TRUE, 'Erdő');
-VALUES ('Tudományos előadás', 'Előadás a fizika világáról', '2026-10-01 14:00:00', '2026-10-01 16:00:00', 'Beltéri', FALSE, 'Aula');
-VALUES ('Sportnap', 'Egész napos sportesemény a pályán', '2026-11-20 09:00:00', '2026-11-20 17:00:00', 'Kültéri', TRUE, 'Sportpálya');
+VALUES
+    ('Iskolai kirándulás', 'Kirándulás a közeli erdőbe', '2026-09-15 08:00:00', '2026-09-15 18:00:00', 'Kültéri', TRUE, 'Erdő'),
+    ('Tudományos előadás', 'Előadás a fizika világáról', '2026-10-01 14:00:00', '2026-10-01 16:00:00', 'Beltéri', FALSE, 'Aula'),
+    ('Sportnap', 'Egész napos sportesemény a pályán', '2026-11-20 09:00:00', '2026-11-20 17:00:00', 'Kültéri', TRUE, 'Sportpálya'),
+    ('Karácsonyi műsor', 'Ünnepi fellépések és versek', '2026-12-18 10:00:00', '2026-12-18 13:00:00', 'Beltéri', TRUE, 'Tornaterem'),
+    ('Matematikaverseny', 'Iskolai szintű matematikaverseny', '2026-10-22 08:00:00', '2026-10-22 12:00:00', 'Beltéri', FALSE, 'Könyvtár'),
+    ('Szülői értekezlet', 'Évközi szülői értekezlet minden osztálynak', '2026-11-05 17:00:00', '2026-11-05 19:00:00', 'Beltéri', TRUE, 'Aula'),
+    ('Tavaszi takarítónap', 'Közös iskolaudvar-rendezés', '2026-04-22 09:00:00', '2026-04-22 13:00:00', 'Kültéri', FALSE, 'Iskolaudvar'),
+    ('Egészségnap', 'Egészséges életmóddal kapcsolatos programok', '2026-05-10 08:00:00', '2026-05-10 16:00:00', 'Beltéri', FALSE, 'Tornaterem');
