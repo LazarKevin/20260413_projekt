b3008fc1b357e897979518e4d73ce638670c6654
Levi-pg <kis.janos.levente@diak.szbi-pg.hu>
2026-04-20 10:58:04 +0200

    Add files via upload

---
+++ C#/elfogad.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace regElfogad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connStr = "server=localhost;database=adatok;uid=root;pwd=mysql;";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string sql = "Select * from regist";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellEventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+            button1.Visible=false;
+            button2.Visible=false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+            }
+            button2.Visible= false; button1.Visible=false;
+        }
+    }
+}
